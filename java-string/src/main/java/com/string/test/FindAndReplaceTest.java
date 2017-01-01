9a6e0be1b5b228bb4d28bdba400ab400417b3f40
solutionmind <rajesh.kumar.mca.cu@gmail.com>
2017-01-01 21:50:37 +0530

    removed un necessary code

--- java-string/src/main/java/com/string/test/FindAndReplaceTest.java
+++ java-string/src/main/java/com/string/test/FindAndReplaceTest.java
@@ -4,8 +4,8 @@ public class FindAndReplaceTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String original = "vfszip:/C:/jboss-5.1/server/default/deploy/cbvm.war/WEB-INF/lib/services-client-1.0.jar/com/cox/webstrategy/services/client/MiddlewareServer.class";
-		original.replaceAll("cbvm.war", "cbvm-services.war");
+		String original = "";
+		original.replaceAll("", "");
 		
 		System.out.println(original);
 
